dab4235d80e11f3733ee9aefe73be6d167305f3d
evoup <evoex123@gmail.com>
2016-12-11 21:43:46 +0800

    add file

---
+++ src/main/java/org/beihang/bigData/CharImageProcess.java
@@ -0,0 +1,8 @@
+package org.beihang.bigData;
+
+/**
+ * Created by evoup on 16-12-11.
+ */
+public interface CharImageProcess {
+    public String getTextFromSpiderImage(final String receiptImageFilePath);
+}
---
+++ src/main/java/org/beihang/bigData/CharImageProcessImpl.java
@@ -0,0 +1,11 @@
+package org.beihang.bigData;
+
+/**
+ * Created by evoup on 16-12-11.
+ */
+public class CharImageProcessImpl implements CharImageProcess {
+    @Override
+    public String getTextFromSpiderImage(String receiptImageFilePath) {
+        return null;
+    }
+}
