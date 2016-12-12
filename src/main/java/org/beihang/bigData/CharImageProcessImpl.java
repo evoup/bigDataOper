ca61c04628f16a09209d830b2905c40e2ef102cc
evoup <evoex123@gmail.com>
2016-12-12 22:11:36 +0800

    c

--- src/main/java/org/beihang/bigData/CharImageProcessImpl.java
+++ src/main/java/org/beihang/bigData/CharImageProcessImpl.java
@@ -7,6 +7,7 @@ import org.apache.commons.logging.LogFactory;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -35,14 +36,55 @@ public class CharImageProcessImpl implements CharImageProcess {
         BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = img.createGraphics();
         g2d.setFont(font);
-        FontMetrics fm = g2d.getFontMetrics();
-        /*int width = fm.stringWidth(text);
-        int height = fm.getHeight();*/
-        int width = 100;
-        int height = 100;
+
+
+        AffineTransform stretch = new AffineTransform();
+        int w = 640; // image width
+        int h = 200; // image height
+        int f = 21; // Font size in px
+        String s = "The quick brown fox jumps over the lazy dog.";
+
+        final BufferedImage bi = new BufferedImage(
+                w,h,BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bi.createGraphics();
+        g.setFont(new Font("Serif",Font.PLAIN,f));
+        g.setRenderingHint(
+                RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        // paint BG
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, w, h);
+        g.setColor(Color.BLACK);
+
+        for (int i=0; (i*f)+f<=h; i++) {
+            g.drawString(s, 0, (i*f)+f);
+            // stretch
+            stretch.concatenate(
+                    AffineTransform.getScaleInstance(1.18, 1d));
+            g.setTransform(stretch);
+
+            // fade
+            Color c = g.getColor();
+            g.setColor(new Color (
+                    c.getRed(),
+                    c.getGreen(),
+                    c.getBlue(),
+                    (int)(c.getAlpha()*.75)));
+        }
+
+        g.dispose();
+
+
+
+
+
+/*        FontMetrics fm = g2d.getFontMetrics();
+        int width = fm.stringWidth(text);
+        int height = fm.getHeight();
         g2d.dispose();
         img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        g2d = img.createGraphics();
+        g2d = img.createGraphics();*/
 /*        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
@@ -51,12 +93,12 @@ public class CharImageProcessImpl implements CharImageProcess {
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);*/
-        g2d.setFont(font);
+/*        g2d.setFont(font);
         g2d.translate(100,100);
         fm = g2d.getFontMetrics();
         g2d.setColor(Color.BLACK);
         g2d.drawString(text, 0, fm.getAscent());
-        g2d.dispose();
+        g2d.dispose();*/
         try {
             LOG.info("[start write png]");
             //ImageIO.write(img, "png", new File("/sparkStream001/Text.png"));
