403ba4bb8b418e8c95bae78d1806e9756dfd1991
evoup <evoex123@gmail.com>
2016-12-12 13:37:07 +0800

    c

--- src/main/java/org/beihang/bigData/CharImageProcessImpl.java
+++ src/main/java/org/beihang/bigData/CharImageProcessImpl.java
@@ -3,6 +3,12 @@ package org.beihang.bigData;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by evoup on 16-12-11.
  */
@@ -18,9 +24,38 @@ public class CharImageProcessImpl implements CharImageProcess {
 
     @Override
     public String getTextFromSpiderImage(String receiptImageFilePath) {
+        String text = "1";
         Fonts fonts = new Fonts();
         LOG.info("[new Fonts]");
-        fonts.getFont(receiptImageFilePath);
+        Font font = fonts.getFont(receiptImageFilePath);
+        BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
+        g2d.setFont(font);
+        FontMetrics fm = g2d.getFontMetrics();
+        int width = fm.stringWidth(text);
+        int height = fm.getHeight();
+        g2d.dispose();
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        g2d = img.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+        g2d.setFont(font);
+        fm = g2d.getFontMetrics();
+        g2d.setColor(Color.BLACK);
+        g2d.drawString(text, 0, fm.getAscent());
+        g2d.dispose();
+        try {
+            LOG.info("[start write png]");
+            ImageIO.write(img, "png", new File("Text.png"));
+        } catch (IOException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
         return null;
     }
 
--- src/test/java/CharImageProcessTest.java
+++ src/test/java/CharImageProcessTest.java
@@ -3,6 +3,8 @@ import org.beihang.bigData.CharImageProcess;
 import org.beihang.bigData.CharImageProcessImpl;
 import org.beihang.bigData.Fonts;
 
+import java.awt.*;
+
 /**
  * Created by evoup on 16-12-11.
  */
@@ -16,7 +18,7 @@ public class CharImageProcessTest extends TestCase {
 
     public void testGetFont() {
         Fonts fonts = new Fonts();
-        fonts.getFont("/project/1.ttf");
+        Font font = fonts.getFont("/project/1.ttf");
         assertTrue(true);
     }
 
