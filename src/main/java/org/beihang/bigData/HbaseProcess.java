a26c17bbc071a04a81076b38327fbf53efacb7a5
evoup <evoex123@gmail.com>
2016-12-16 15:23:57 +0800

    write hbase

--- src/main/java/org/beihang/bigData/HbaseProcess.java
+++ src/main/java/org/beihang/bigData/HbaseProcess.java
@@ -14,15 +14,16 @@ import java.io.IOException;
 public class HbaseProcess {
 
     public void saveImg(String fontName) throws IOException {
-
-        Configuration config = HBaseConfiguration.create();
-        HTable table = new HTable(config, "ocr_table");
+        Configuration hBaseConfig =  HBaseConfiguration.create();
+        hBaseConfig.setInt("timeout", 120000);
+        hBaseConfig.set("hbase.master", "*" + "zoo3" + ":9000*");
+        hBaseConfig.set("hbase.zookeeper.quorum","zoo3");
+        hBaseConfig.set("hbase.zookeeper.property.clientPort", "2181");
+        HTable table = new HTable(hBaseConfig, "ocr_table");
         Put p = new Put(Bytes.toBytes(fontName));
         p.add(Bytes.toBytes("info"), Bytes.toBytes("type"),
                 Bytes.toBytes("png"));
         table.put(p);
-
-
     }
 
 
