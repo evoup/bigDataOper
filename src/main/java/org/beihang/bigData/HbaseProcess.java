8c3a624fb9645ade415703b38ebd30ecca4853eb
evoup <evoex123@gmail.com>
2016-12-16 14:58:53 +0800

    add hbase support

--- src/main/java/org/beihang/bigData/HbaseProcess.java
+++ src/main/java/org/beihang/bigData/HbaseProcess.java
@@ -2,10 +2,26 @@ package org.beihang.bigData;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
 
 /**
  * Created by evoup on 16-12-16.
  */
 public class HbaseProcess {
-    Configuration config = HBaseConfiguration.create();
+
+    public void saveImg(String fontName) throws IOException {
+
+        Configuration config = HBaseConfiguration.create();
+        HTable table = new HTable(config, "ocr_table");
+        Put p = new Put(Bytes.toBytes(fontName));
+        p.add(Bytes.toBytes("myLittleFamily"), Bytes.toBytes("someQualifier"),
+                Bytes.toBytes("Some Value"));
+
+    }
+
+
 }
