ace484a04d38067102d0170887b78eae7e4e7c30
evoup <evoex123@gmail.com>
2016-12-16 13:51:13 +0800

    add hbase support

---
+++ src/main/java/org/beihang/bigData/HbaseProcess.java
@@ -0,0 +1,11 @@
+package org.beihang.bigData;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+
+/**
+ * Created by evoup on 16-12-16.
+ */
+public class HbaseProcess {
+    Configuration config = HBaseConfiguration.create();
+}
