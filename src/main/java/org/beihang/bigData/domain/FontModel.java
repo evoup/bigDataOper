6473abdcbda37b15d2a6274bf84daa8dfa5c681b
evoup <evoex123@gmail.com>
2016-12-22 01:42:56 +0800

    add FontModel

---
+++ src/main/java/org/beihang/bigData/domain/FontModel.java
@@ -0,0 +1,19 @@
+package org.beihang.bigData.domain;
+
+import java.awt.*;
+
+/**
+ * Created by evoup on 2016/12/22.
+ */
+public class FontModel {
+    String name;
+    Font font;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
