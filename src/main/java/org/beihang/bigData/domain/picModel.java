bcbdb2b9b21602dc348037fa81f922d37c19f43f
evoup <evoex123@gmail.com>
2016-12-22 23:00:30 +0800

    add lost file

---
+++ src/main/java/org/beihang/bigData/domain/picModel.java
@@ -0,0 +1,30 @@
+package org.beihang.bigData.domain;
+
+/**
+ * Created by evoup on 2016/12/22.
+ */
+public class PicModel {
+    String fontName;
+    String base64Content;
+
+    public PicModel(String fontName, String base64Content) {
+        this.fontName = fontName;
+        this.base64Content = base64Content;
+    }
+
+    public String getFontName() {
+        return fontName;
+    }
+
+    public void setFontName(String fontName) {
+        this.fontName = fontName;
+    }
+
+    public String getBase64Content() {
+        return base64Content;
+    }
+
+    public void setBase64Content(String base64Content) {
+        this.base64Content = base64Content;
+    }
+}
